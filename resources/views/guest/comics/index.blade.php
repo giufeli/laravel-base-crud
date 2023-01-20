8e745bb7fbfae7c31a6cf80ff7dfc80f096f3d60
giufeli <giuliofelicetti99@gmail.com>
2023-01-20 17:11:45 +0100

    Commit

---
+++ resources/views/guest/comics/index.blade.php
@@ -0,0 +1,30 @@
+@section('content')
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Link</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di vendita</th>
+                <th scope="col">Tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->id }}</th>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->description }}</td>
+                    <td>{{ $comic->thumb }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->type }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
